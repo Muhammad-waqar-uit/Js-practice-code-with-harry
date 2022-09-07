1a95cb4d7c96e7b967ddf4326b21befd7a5f3eb2
Muhammad Waqar <57596726+Muhammad-waqar-uit@users.noreply.github.com>
2022-09-07 11:50:03 +0000

    index file

---
+++ index.js
@@ -0,0 +1,22 @@
+//chapter 1
+//question1
+let a = 'waqar';
+let b = 1;
+console.log(a + b)
+//question2
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof (a + b))
+
+//question 3
+const d = {
+  'waqar': 3,
+  'nice': 'yes',
+  2: 33
+}
+
+// object variable cannot be re assigned.
+
+d['nam2']='hello'
+
+console.log(d)
