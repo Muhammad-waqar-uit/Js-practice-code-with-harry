37d7eb52ca42165501a14fad78e709a472efa742
Muhammad Waqar <MW6069605@GMAIL.COM>
2022-09-15 18:59:33 +0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,18 @@
+/*
+function factorial(number){
+    let n=1;
+    for (i=1;i<=number;i++){
+        number = n*=i;
+        console.log(number);
+    }
+}
+factorial(3)
+*/
+let obj={
+    'waqar':34,
+    'sumail':33,
+    'topson':23
+}
+for (a in obj){
+    console.log(obj[a]);
+}
---
+++ while.js
@@ -0,0 +1,15 @@
+// n=0;
+// let i=0;
+
+// while(i<n){
+//     console.log(i);
+//     i++;
+// }
+
+// //atleaset one time loop
+// n=4;
+// let i=4;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<n);
