59be844425c9f2f9679542bafd9bfbb25b8cf155
Muhammad Waqar <MW6069605@GMAIL.COM>
2022-09-15 19:40:49 +0500

    Add files via upload

---
+++ practice loops.js
@@ -0,0 +1,24 @@
+// q1
+// let marks={
+//     'waqar':33,
+//     'check1':35,
+//     'check2':34
+// }
+
+// for (let i=0;i<Object.keys(marks).length;i++){
+//     console.log('The marks of '+Object.keys(marks)[i]+' are ' +marks[Object.keys(marks)[i]]);
+// }
+//part2
+// for (a in marks){
+// console.log('The marks of '+ a+' are '+ marks[a])
+// }
+//part3
+// let cn=5;
+// let i;
+// while(i!=cn){
+//     i=prompt('enter the correct number: ');
+// }
+//part 4
+const mean = (b, c, d, e) =>{
+    return (b+c+d+e)/4
+}
