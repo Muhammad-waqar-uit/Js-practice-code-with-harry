555bf9ee3d8b9e6d5c6857f580b90363a5490707
Muhammad Waqar <MW6069605@GMAIL.COM>
2022-09-15 19:08:35 +0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function _name(name){
+    console.log('i am good nice meeting you : ',name);
+}
+
+// const _name=(name)=>{
+//     console.log(name);
+// }
+_name('waqar')
+
+const sum=(p,q)=>{
+    return p+q;
+}
+console.log(sum(6,7));
